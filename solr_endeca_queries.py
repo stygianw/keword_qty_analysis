62a9e3d2c6b8f6526afcfd3cea16162b90347659
stygianw <stygianwatcher@gmail.com>
2019-08-15 16:34:28 +0300

    New Solr query form to fix phrase boost

--- solr_endeca_queries.py
+++ solr_endeca_queries.py
@@ -3,7 +3,7 @@ from xml.etree import ElementTree as ET
 import re
 
 
-SOLR_QUERY = "http://dev.bradfordexchange.com:8983/solr/US_items_core/select?f.Themes_TREE_2_facet.facet.mincount=1&df=_text_&f.Product_Types_TREE_1_facet.facet.mincount=1&bf=field(popularityBoost)&f.More_Ways_To_Shop_TREE_0_facet.facet.mincount=1&fq=DISPLAY:Y&fq=STORE_AVAILABLE:Y&fq=MARKET_CLASS:P&autocorrection.spellmode=ALL&f.Themes_TREE_0_facet.facet.mincount=1&defType=edismax&spellcheck.q=disney&qf=EXTERNAL_PRODUCT_IDS_search^200.0+title_search^240.0+SHORT_NAME_search^190.0+shortdesc_search^210.0+FEATURE_VALUES_search^200.0+KEYWORDS_search^190.0+MANUFACTURER^20.0+ARTIST_search^1.0+OTHER_COUNTRY_PROD_ID^0.8+code_search^0.7+EMAIL_SUBJECT_TITLE_search+Product_Types_TREE_fsearch^240.0+Themes_TREE_fsearch^160.0&wt=xml&facet.field=Themes_TREE_0_facet&facet.field=Themes_TREE_1_facet&facet.field=Themes_TREE_2_facet&facet.field=Themes_TREE_3_facet&facet.field=Product_Types_TREE_0_facet&facet.field=Product_Types_TREE_1_facet&facet.field=Product_Types_TREE_2_facet&facet.field=More_Ways_To_Shop_TREE_0_facet&facet.field=More_Ways_To_Shop_TREE_1_facet&facet.field=More_Ways_To_Shop_TREE_2_facet&f.Product_Types_TREE_2_facet.facet.mincount=1&f.More_Ways_To_Shop_TREE_1_facet.facet.mincount=1&start=0&f.Themes_TREE_1_facet.facet.mincount=1&rows=10000&f.Product_Types_TREE_0_facet.facet.mincount=1&spellcheck=true&pf=EXTERNAL_PRODUCT_IDS_search^200.0+title_search^240.0+SHORT_NAME_search^190.0+shortdesc_search^210.0+FEATURE_VALUES_search^200.0+KEYWORDS_search^190.0+MANUFACTURER^20.0+ARTIST_search^1.0+OTHER_COUNTRY_PROD_ID^0.8+code_search^0.7+EMAIL_SUBJECT_TITLE_search&f.Themes_TREE_3_facet.facet.mincount=1&f.More_Ways_To_Shop_TREE_2_facet.facet.mincount=1&facet=on"
+SOLR_QUERY = "http://dev.bradfordexchange.com:8983/solr/US_items_core/select?f.Themes_TREE_2_facet.facet.mincount=1&df=_text_&f.Product_Types_TREE_1_facet.facet.mincount=1&bf=field(popularityBoost)&f.More_Ways_To_Shop_TREE_0_facet.facet.mincount=1&fq=DISPLAY:\"Y\"&fq=STORE_AVAILABLE:\"Y\"&fq=MARKET_CLASS:\"P\"&autocorrection.spellmode=ALL&f.Themes_TREE_0_facet.facet.mincount=1&defType=edismax&spellcheck.q=college+sports&qf=EXTERNAL_PRODUCT_IDS_search^200.0+title_search^330.0+SHORT_NAME_search^230.0+shortdesc_search^170.0+FEATURE_VALUES_search^160.0+KEYWORDS_search^150.0+MANUFACTURER^20.0+ARTIST_search^60.0+OTHER_COUNTRY_PROD_ID^0.8+code_search^0.7+EMAIL_SUBJECT_TITLE_search^10.0+Product_Types_TREE_fsearch^220.0+Themes_TREE_fsearch^200.0&wt=xml&facet.field=Themes_TREE_0_facet&facet.field=Themes_TREE_1_facet&facet.field=Themes_TREE_2_facet&facet.field=Themes_TREE_3_facet&facet.field=Product_Types_TREE_0_facet&facet.field=Product_Types_TREE_1_facet&facet.field=Product_Types_TREE_2_facet&facet.field=More_Ways_To_Shop_TREE_0_facet&facet.field=More_Ways_To_Shop_TREE_1_facet&facet.field=More_Ways_To_Shop_TREE_2_facet&f.Product_Types_TREE_2_facet.facet.mincount=1&f.More_Ways_To_Shop_TREE_1_facet.facet.mincount=1&start=0&f.Themes_TREE_1_facet.facet.mincount=1&rows=10000&f.Product_Types_TREE_0_facet.facet.mincount=1&spellcheck=true&pf=EXTERNAL_PRODUCT_IDS_search^200.0+title_search^330.0+SHORT_NAME_search^230.0+shortdesc_search^170.0+FEATURE_VALUES_search^160.0+KEYWORDS_search^150.0+MANUFACTURER^20.0+ARTIST_search^60.0+OTHER_COUNTRY_PROD_ID^0.8+code_search^0.7+EMAIL_SUBJECT_TITLE_search^10.0&f.Themes_TREE_3_facet.facet.mincount=1&f.More_Ways_To_Shop_TREE_2_facet.facet.mincount=1&facet=on"
 
 
 def _create_solr_url(search_word):
@@ -35,13 +35,13 @@ def find_solr_props(search_word):
 
 def _create_solr_search_string(solr_query):
     if re.match(r'\w+\s+\w+', solr_query):
-        return "({})(\"{}\")".format(re.sub(r'(\w+)', r'+\1', solr_query), solr_query).lower()
+        return "({}) \"{}\"".format(re.sub(r'(\w+)', r'+\1', solr_query), solr_query).lower()
     else:
         return solr_query.lower()
 
 
 def test_create_solr_query():
-    assert _create_solr_search_string('BEAUTY AND THE BEAST') == '(+beauty +and +the +beast)("beauty and the beast")'
+    assert _create_solr_search_string('BEAUTY AND THE BEAST') == '(+beauty +and +the +beast) "beauty and the beast"'
     assert _create_solr_search_string('FAIRIES') == "fairies"
 
 
@@ -50,5 +50,5 @@ def _create_solr_q_param(search_string):
 
 
 def test_create_solr_q_param():
-    assert _create_solr_q_param("BEAUTY AND THE BEAST") == "q=%2B((%2Bbeauty+%2Band+%2Bthe+%2Bbeast)(\"beauty+and+the+beast\"))"
+    assert _create_solr_q_param("BEAUTY AND THE BEAST") == "q=%2B((%2Bbeauty+%2Band+%2Bthe+%2Bbeast)+\"beauty+and+the+beast\")"
 
